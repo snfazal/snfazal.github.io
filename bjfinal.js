f1ffbb9e7d04f1d7241f47af3aaa390534d05399
Sophia Fazal <Sophie@Sophias-MacBook-Pro.local>
2016-11-28 13:16:33 -0500

    what's left

--- bjfinal.js
+++ bjfinal.js
@@ -2,10 +2,10 @@ console.log('hello world, its time play blackjack');
 
   $(function(){ //window.onload jQuery style
       // collecting all buttons
-      var $hitButton = $('#hit');
-      var $stayButton = $('#stay');
-      var $clearButton = $('#clear');
-      var $resetButton = $('#reset');
+      var $hitButton = $('#hit'); //MVP
+      var $stayButton = $('#stay'); //MVP
+      var $clearButton = $('#clear'); //ND essentially start new game/clear hand
+      var $resetButton = $('#reset'); //ND
 
 
 //    *Create game statistics and objects
@@ -16,11 +16,37 @@ console.log('hello world, its time play blackjack');
 
        // object constructor needed to make each new card
        var Card = function (suit, value, rank) {
+          // this.suit = suit;
           this.face = face;
           this.value = value;
           this.rank = rank;
        },
 
+       // object that makes deck
+       var makeDeck = function { //array to hold cards
+         cards: []; //empty
+         faceCards: ['hearts', 'spades', 'diamonds', 'clubs'], //face card values
+         faceRanks: ['Jack', 'Queen', 'King'], //face card ranks
+         numberValues: [2, 3, 4, 5, 6, 7, 8, 9, 10], //card values
+         aceValue: ['Ace'], //ace identifies with ace only
+
+
+      makeNumbers
+
+
+       }
+
 
 
       });
+
+
+//////////////////////////////////////////////////////////////////////////////
+
+// first draft 
+
+
+
+
+
+}
--- blackjack.js
+++ blackjack.js
@@ -1,6 +1,8 @@
 console.log('hello world, its blackjack');
 //DEAL OUT CARDS
 
+//I struggled with using cache selectors, so I used OOP in order to help me get things into my HTML
+
 $(function(){ //window.onload, loads functionality within the window
 
 
@@ -74,10 +76,15 @@ $(function(){ //window.onload, loads functionality within the window
       })
 
 
-    })
+    }) //end window.onload
 
     // })//
 
+    //originally three parameters, but hand value can double as score
+  var Card = function(suit, value) { //constructor function that creates cards one at a time
+    this.suit = suit; //parameter suit (out of four )
+    this.value = value; //parameter value
+  }; //end constructor
 
 
 //////////////////////////////////////////////////////////////////
@@ -122,17 +129,17 @@ $(function(){ //window.onload, loads functionality within the window
      return this.deck;
    }, //end makeDeck.shuffle
 
+ };//end of deck object
 
-   makeItWork: function() {
+   makeItWork = function() {
      deck.makeValueCards();
      deck.makeFaceCards();
      deck.shuffle();
      }
 
-  } //end of deck object
+
  deck.makeItWork();
 
-};
 
 /* Without Blaine the functionality below wouldn't work, I would be lost and out of WDI and my life would simply be over. His exact words were "...when you say reorder can you clarify that alil, and you dont have to put this. in front of the methods, but they can go inside the methods like this.hitMe would just be hitMe: and this. would go in front of cards like this.cards.length. this refers to the player object. your code looks though ! I'll keep checking it out to see if i can find anything..."
 
@@ -145,20 +152,20 @@ As you can see, the below code, looks cleaner, it functions, I understand it(not
        score = 0, //keeps score by value of cards
        cardVal = 0, //stores value of cards in hand
        aces = 0; //stores number of aces in the hand
-       for (i=0; i < this.hand.length; i++) { //phandtotal.phand through the array
+       for (i=0; i < this.hand.length; i++) { //handtotal.hand through the array
          var value = this.hand[i].value; //generates value of above
          if (value <= 10) { //if value is 11 then ace is 1
              aces += 1;
       }
       score += cardVal; //
-     }
+      }
       //Checks to see if Aces should be 1 or 11 (DM)
       while (score > 21 && aces > 0){ //while # is bw 21 and 0
           score -= 10; //
           aces -=1;
-    }
+      }
       return score; //returns aces value based on cards.score function
-  },
+      },
 
       //method to hit
     hitMe: function() {
@@ -243,11 +250,11 @@ As you can see, the below code, looks cleaner, it functions, I understand it(not
       document.getElementById('showDealerHand').innerHTML = this.handValue();
     },
 
-    start: function() {
-      var timer = this;
-      this.dealerTimer = setInterval(function(){
-        self.showHand();
-      }, 6000);
+    start: function() { //method for setting timer
+      var Timer = this; //this will prevent everything form running all at once, dealer can deal on care at a time
+      this.dealerTimer = setInterval(function(){ //set interval function
+        self.showHand(); //shows current hand after running timer
+      }, 6000); //timer runns every 6 seconds
     }
 
   };
@@ -278,352 +285,6 @@ var bank = {
 clearInputBox: function() { //method clears the input box where bets are made
   var bankBox = document.getElementById('money'); //clears money for next game/hand to be played
   bankBox.value = ''; //input box in bank
-}
-
-
+   }
 
 };
-
-
-  // //returns array with a list of the names (suits/faces/values) of the cards in the players hand
-  // this.nameHand = function() {
-  //   var yourCardsAre = [],
-  //   i;
-  //
-  //   for (i = 0; i < cards.length; i++){ //must get through all the cards in hand
-  //       yourCardsAre.push(cards[i].getName());
-  //   }
-  //   return yourCardsAre.join(); //literally tells you what cards you're holding in hand
-  // };
-  //
-  // //meethod to hit
-  // this.hitMe = function() { //adds card from deck to hand at play
-  //   // console.log('what is happening here');
-  //   if (cards.length < 5){
-  //       cards.push(deck.deal());
-  //   }
-  // };
-  // //grabs the HTML id of the cards in players hand
-  // this.toHTML = function(){
-  //   var yourCardsAre = [],
-  //       i;
-  //
-  //   for (i = 0; i < cards.length; i++){
-  //       arrayOut.push('<div class="card ', cards[i].getSuit(),' ',cards[i].getNumber(),'">',cards[i].getName(),'</div>');
-  //   }
-  //   return arrayOut.join('');
-  // };
-
-}); //end window onload
-
-
-
-
-//    // Returns the HTML coded symbol of card suit
-//    //set up idea from codeacademy game
-//       this.makeFace = function (){
-//         var suitName = '';
-//           switch (suit){
-//             case 1:
-//                 suitName = "&hearts;";
-//                 break;
-//             case 2:
-//                 suitName = "&clubs;";
-//                 break;
-//             case 3:
-//                 suitName = "&spades;";
-//                 break;
-//             case 4:
-//                 suitName = "&diamonds;";
-//                 break;
-//           }
-//           return suitName;
-//         };
-//         //Turns number into correct blackjack value to keep score
-//         this.getValue = function() {
-//             var value = number;
-//             if (number >= 10){
-//                 value = 10;
-//             } if (number === 1) {
-//                  value = 11;
-//             } return value;
-//           };
-//       // returns face cards and regular numbers
-//       this.getNumber = function() {
-//         var cardName = '';
-//           switch(number) {
-//             case 1:
-//                 cardName = "Ace";
-//             case 11:
-//                 cardName = "Jack";
-//             case 12:
-//                 cardName = "Queen";
-//             case 13:
-//                 cardname = "King";
-//             default:
-//                 cardName = number;
-//          }
-//          return cardName+this.getSymbol();
-//       };
-//
-// //Constructor Function:
-//   //Function: creates a random card
-// var Deck = function() {
-//   var cards = [];
-//   //below creates new SET of cards
-//   var newCards = function() {
-//     var i, suit, number;
-//     for (i=0; i < 52; i++) {
-//         suit = i % 4 + 1;
-//         cards.push(new Card(suit, number));
-//     }
-//   };
-//   //create new cards
-//   newCards();
-//    //shuffle below found on stackoverflow
-//   this.shuffle = function() { //method to shuffle deck of cards
-//     for(var j, x, i = cards.length; i; j = parseInt(math.random() * i), x = cards[--i], cards[i] = cards[j], cards[j] = x); //shuffle must go through whole deck
-//     return cards; //returns shuffled cards
-//   };
-//
-//   //Array of cards representing the Deck
-//   this.getCards = function() {
-//     return cards;
-//   };
-//   this.deal = function() {
-//     if (!cards.length){
-//        console.log(' no more cards, new deck ');
-//        newCards();
-//        this.shuffle();
-//      }
-//      return cards.pop();
-//   };
-// };
-
-//Constructor Function:
-// var Hand = function(deck) {
-//   var cards = [];
-//
-//      //Deal one card for player and one for dealer to start game
-//      cards.push(deck.deal(), deck.deal());
-//      //returns array of cards that represents the Hand
-//      this.getHand = function() {
-//        return cards;
-//    };
-//      //returns the value of the hand as the score
-//      this.score = function() {
-//        var i,
-//            score = 0, //keeps score by value of card
-//            cardVal = 0, //stores cards value
-//            aces = 0; //stores number of aces in the hand (later to detemine value)
-//        for (i=0; i<cards.length; i++){
-//            cardVal = cards[i].getValue();
-//            if (cardVal == 11) {
-//                aces += 1;
-//          }
-//          score += cardVal;
-//       }
-//       //Checks to see if Aces should be 1 or 11 (ask matt why this way best?? makes game easier?)
-//       //*
-//     while (score > 21 && aces > 0){
-//           score -= 10;
-//           aces -=1;
-//     }
-//       return score;
-//   };
-//   //returns array list of Card names in hand
-//   this.printHand = function() {
-//     var arrayOut = [],
-//     i;
-//
-//     for (i = 0; i < cards.length; i++){ //must get through all the cards in hand
-//         arrayOut.push(cards[i].getName());
-//     }
-//     return arrayOut.join(); //literally tells you what you're holding
-//   };
-//
-//   //Adds a card from the Deck into players hands
-//   this.hitMe = function() {
-//     // console.log('what is happening here');
-//     if (cards.length < 5){
-//         cards.push(deck.deal());
-//     }
-//   };
-//   //grabs the HTML image of the cards in hand at player
-//   this.toHTML = function(){
-//     var arrayOut = [],
-//         i;
-//
-//     for (i = 0; i < cards.length; i++){
-//         arrayOut.push('<div class="card ', cards[i].getSuit(),' ',cards[i].getNumber(),'">',cards[i].getName(),'</div>');
-//     }
-//     return arrayOut.join('');
-//   };
-
-
-
-
-//Playing the game BLACKJACK
-// $(function(){
-//
-// var blackJack = $(function (){
-//
-//     var deck = new Deck(); //new deck for game (multiple decks MVP)
-//
-//     var wins = 0; //winning ratios
-//     var losses = 0; //initially probability should be the same
-//
-// var declareWinner = function(playerHand, dealerHand){ //keeps score the declare the winner of each hand
-//     var outcome = '', //end score
-//     dealerScore = dealerHand.score(), //dealers hand out is his score
-//     playerScore = playerHand.score(); //players' hand out is her score
-//
-//     //if statement stating original blackjack game rules
-//     if(playerScore > 21 || dealerScore === 21){ //pS less than dealer who has 21
-//       outcome = " Sorry, You Lose! "; //winning outcome message
-//         loses++; //player loses
-//     }else if (playerScore <= 21 && playerHand.getHand().length >= 5){ //less than or equal to 21 with 5 cards total
-//       outcome = " Yay! You win! "; //winning outcome message
-//       wins++; //player wins
-//     }else if (dealerScore > 21 || playerScore === 21 || playerScore > dealerHand.score()){ // dealer has less than 21 or player has 21
-//       outcome = " You Win! "; //winning outcome message
-//       wins++; //player wins
-//     }else if (dealerScore > playerScore){ //dealer score is higher than players
-//       outcome = " :( You lose! "; //winning outcome message
-//       loses++; //player loses
-//     }else if (dealerScore === playerScore){
-//       outcome = " it's a tie ";
-//       //losses++; //player loses bc he doesn't win
-//     }
-    //return code outputs score of the round. <br/> breaks it up in HTML, adds dealers hand score <br/> and adds the players score. Makes it all work in DOM
-    // return outcome+"<br />Dealer: "+dealerHand.score()+"<br />You "+playerScore;
-    // };
-
-//Dealer's Hand
-//  var dealerHand = function() {
-//       var hand = new Hand(deck); //new deck for the dealers hand
-//       //while loop below makes dealer continue to hit when hand value bw 1-17
-//       while (hand.score() < 17 && hand.length < 1) {
-//       hand.hitMe(); //hitMe button for hand equation above
-//       }
-//       return hand; //returns value of the dealers hand
-//     };
-//
-//     var yourHand;
-//
-// //CACHE SELECTORS
-//     var $hitButton = $("#hitMe"),
-//         $standButton = $("#stand"),
-//         $dealButton = $("#deal"),
-//         $score = $("#yourScore"),
-//         $yourHand = $('#yourHand'),
-//         $dealerHand = $('#dealerHand');
-//
-// //function that shows deal button and hides stand and hit
-//     var showDeal = function() {
-//       $hitButton.hide();
-//       $standButton.hide();
-//       $dealButton.show();
-//     };
-//
-//     var showControls = function() {
-//        $hitButton.show();
-//        $standButton.show();
-//        $dealButton.show();
-//     };
-//
-//     var updateUI = function() {
-//       $yourHand.html(yourHand)
-//     };
-
-
-
-// }); //closes window.onload function
-
-
-  // var deal = function() {
-  //   //Private local variables
-  //   var s = Math.floor(Math.random() * 4 + 1);
-  //   var n = Math.floor(Math.random() * 13 + 1);
-  //
-  //   return new Card(s, n);
-  // };
-  //
-  // //Constructor: Sets up cards and related methods
-  // function Hand() {
-  //   //Private local variables
-  //   var card1 = deal();
-  //   var card2 = deal();
-  //   var totalScore = card1.getValue +card2.getValue();
-  //   var nextCard = 3;
-  //
-  //   //Returns the amount of the cards in play
-  //   this.getCardAmt = function() {
-  //     return nextCard - 1;
-  //   };
-  //
-  //   //Adds new card and updates total score for a "hit"
-  //   this.hit = function() {
-  //     switch(nextCard) {
-  //       case 3:
-  //           var card3.getValue();
-  //           nextCard++;
-  //           break;
-  //       case 4:
-  //         var card4 = deal();
-  //     }
-  //   }
-  //
-  // };
-  //Use OOP to make the deck by taking properties from above to make a constructor function
-
-
-
-// create a function to deal cards (math.random)
-
-// store cards in array; write out names with dashes for understanding. All cards
-
-// store values correlating to order of cards in array above in a new array
-
-/// ---Deal to the dealer
-
-//zero out the dealer
-
-
-// deal out the cards (math.floor/math.random)
-
-// get the total of the dealer
-// create variable for the class of the cards
-
-// add a class to the cards
-// add an attribute to the cardclass
-
-// create an equation for the dealers total;
-    // create if statement for when the dealers total is less than 21
-    // loop that goes through the array of cards
-    //
-
-
-
-
-
-
-
-/// ---Deal to the player
-
-// zero out the player total
-// create variable of player and equal it to zero
-
-  // deal the cards out to player (math.floor/math.random)
-
-/// What happens if the player stands?
-  //add click button for player to decide they want to stand
-
-  //take dealer total again what cards the player has showing
-  //
-
-  ////Keeping adding cards until over 17 or the dealer busts
-
-
-
-/// What happens
