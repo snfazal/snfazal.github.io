d707b5ab2fbc47229fc785729488c4595b55b19e
Sophia Fazal <Sophie@Sophias-MacBook-Pro.local>
2016-12-03 11:41:43 -0500

    psudocode for card object, set up of dealer/player, and dealing out new card

---
+++ newGame/blackjack.js
@@ -0,0 +1,42 @@
+//Blackjack Game WDI Remote Matrix GA --Sophia Fazal
+
+//Deck object will all 52 cards inside of it
+    // name of card
+    // value of card
+    // suit of card/what type of card
+
+
+
+//Empty arrays declared for dealer and player to dealt into the game. These should be global variables so there is no scoping issue in the future.
+    //sets up dealer
+    //sets of player
+
+
+//Function used to deal one new card for the player from the deck, with an image appended to the card. #2
+    //one random card chosen from the deck
+    //next card off deck is above
+    //card pushed on top of hand
+    //assign image url tag to variable
+    //image url appends the element(image) onto next card waiting to be chosen
+
+
+//function to calculate points total in hand at play for player and dealer
+    //define sum
+    //for loop to count all cards through hand length
+        //cards in the hand are
+        //sumed with a method that will calc their value
+        //and return the sum of their hand
+
+//Fuction checking if player or dealer BUST. #4
+    //the points of the player will be calculated by the value of cards in the players hand
+    // if value is greater than 21
+      //the player will see a message that says he busted
+      //return true;
+
+    //dealers points will be calculated by the value of cards in his hand
+    //if the value is greater than 21
+      //player will see a message "Dealer busted you win"
+      //return true;
+      //return false if otherwise
+
+//
