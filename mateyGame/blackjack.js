76f1560811addf0edabfc7df7ec59c96f2f6bdb7
Sophia Fazal <Sophie@sophias-mbp.attlocal.net>
2016-12-28 15:57:11 -0600

    files

---
+++ mateyGame/blackjack.js
@@ -0,0 +1,32 @@
+$(function(){
+
+//grabb objects from bank in jquery
+
+//create a card function
+
+
+//create a deck object 
+
+
+
+//Deck object with all 52 cards inside of it
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
