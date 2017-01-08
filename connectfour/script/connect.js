1491af12836447a1b3837133844fdf5871366f38
snfazal <snfazal@gmail.com>
2017-01-08 14:46:41 -0500

    finished isValidMove method

--- connectfour/script/connect.js
+++ connectfour/script/connect.js
@@ -14,4 +14,35 @@ console.log("hello world");
       this.board = Array(7).fill(null),
       this.player = "b";
       this.gameOver = "false";
+    },
+
+    //method to toggle between players
+    togglePlayer: function(){
+      //starting player is black
+      if (this.player == "b") {
+        //change that player to black
+        this.player = "r";
+        //else change that player to black
+      } else {
+        this.player = "b";
+      }
+    },
+
+    //method to set legal moves
+    isValidMove: function(columnNum){
+      //if the game is over
+      if(!this.gaveOver){
+        //if the columnNum is has room for anymore pieces to be dropped
+        if(!this.board[columnNum] || this.board[columnNum].length < 6){
+          //clear any moves not legal
+          viewEngine.clearFlash();
+          //returns true if piece can be dropped and game continues
+          return true;
+        }
+        //message for invalid move made in viewEngine constructor
+        viewEngine.flashMessage('aw man, no more space here, try another column');
+        return false;
+      }
+      //returns false(gameover)if move not legal
+      return false;
     }
