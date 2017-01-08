6f224e07bf479334695b76a21f6f565a73a3ed41
snfazal <snfazal@gmail.com>
2017-01-08 13:52:57 -0500

    added extra column, created object constructor gameEngine

--- connectfour/script/connect.js
+++ connectfour/script/connect.js
@@ -1,1 +1,11 @@
 console.log("hello world");
+
+
+//object constructor function
+var gameEngine = {
+  //empty array of 7 columns filled with nothing
+  board: Array(7).fill(null),
+  //first player is red
+  player: "r",
+  gameOver: false,
+}
