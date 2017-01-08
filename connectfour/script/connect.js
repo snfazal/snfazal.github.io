496d0ddd9b9effc91c0be99528752d54e0cf42ed
snfazal <snfazal@gmail.com>
2017-01-08 13:56:38 -0500

    method to reset gameboard

--- connectfour/script/connect.js
+++ connectfour/script/connect.js
@@ -2,10 +2,16 @@ console.log("hello world");
 
 
 //object constructor function
-var gameEngine = {
-  //empty array of 7 columns filled with nothing
-  board: Array(7).fill(null),
-  //first player is red
-  player: "r",
-  gameOver: false,
-}
+  var gameEngine = {
+    //empty array of 7 columns filled with nothing
+    board: Array(7).fill(null),
+    //first player is red
+    player: "r",
+    gameOver: false,
+
+    //method to reset the game, clear gameboard
+    resetGame: function(){
+      this.board = Array(7).fill(null),
+      this.player = "b";
+      this.gameOver = "false";
+    }
